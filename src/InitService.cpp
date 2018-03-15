b87e62e9cb870c8c9bc7578dc763ce12b6a70a37
Yang Liu <byliuyang11@gmail.com>
2018-03-15 00:22:29 -0400

    Remove debug messages

--- src/InitService.cpp
+++ src/InitService.cpp
@@ -12,7 +12,6 @@ extern "C" void init_rendering_service(char *ip_address, int port, void (*on_ser
     std::thread serviceThread([ip_address, port, on_service_started] {
         renderingService = new NaviRice::Networking::Services::RenderingService(ip_address, port, "1");
         renderingService->onServiceStarted([on_service_started](){
-            std::cout<< "onServiceStarted" << std::endl;
             on_service_started();
         });
         renderingService->start();
@@ -22,7 +21,6 @@ extern "C" void init_rendering_service(char *ip_address, int port, void (*on_ser
 
 extern "C" void set_new_step_callback(void (*new_step_callback)(Step)) {
     if (renderingService != nullptr) {
-        std::cout<< "set_new_step_callback" << std::endl;
         renderingService->onReceiveNewStep([new_step_callback](Step step) {
             new_step_callback(step);
         });
